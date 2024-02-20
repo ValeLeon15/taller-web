e8c5e4b307de5ba8048cb5a39d187be60e9813cc
ValeLeon15 <vlg1529@gmail.com>
2024-02-20 14:41:34 -0500

    first commit

---
+++ src/main/java/com/example/demo/plantillas/Controladores.java
@@ -0,0 +1,19 @@
+package com.example.demo.plantillas;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/plantillas")
+public class Controladores {
+
+    @GetMapping("/bienvenida")
+    public String bienvenidaContro(){
+        return"bienvenida";
+    }
+
+    @GetMapping("/")
+    public
+
+}
