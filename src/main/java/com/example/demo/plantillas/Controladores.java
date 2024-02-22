02668969e06e6ddbfed707b3df75753a081c28cc
ValeLeon15 <vlg1529@gmail.com>
2024-02-22 17:12:23 -0500

    sdf

--- src/main/java/com/example/demo/plantillas/Controladores.java
+++ src/main/java/com/example/demo/plantillas/Controladores.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/plantillas")
+@RequestMapping("/grupo14/plantillas")
 public class Controladores {
 
     @GetMapping("/bienvenida")
