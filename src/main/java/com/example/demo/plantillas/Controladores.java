bc9fafc6eeb1daeaa4e27ec1033980f2c5a0d056
limosnd <limontero@javeriana.edu.co>
2024-02-29 12:32:03 -0500

    cambio contraldor intento

--- src/main/java/com/example/demo/plantillas/Controladores.java
+++ src/main/java/com/example/demo/plantillas/Controladores.java
@@ -8,9 +8,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/grupo14/plantillas")
 public class Controladores {
 
-    @GetMapping("/bienvenida")
-    public String bienvenidaContro(){
-        return"bienvenida";
+    @GetMapping("/about-us")
+    public String controlador1(){
+        return"about-us";
+    }
+    @GetMapping("/architecture")
+    public String controlador2(){
+        return"architecture\"";
+    }
+    @GetMapping("/deployment")
+    public String  controladores3(){
+        
+        return"deployment";
+    }
+
+    @GetMapping("/for-development")
+    public String controladores4(){
+        return"for-development";
+    }
+    @GetMapping("/form")
+    public String controladores5(){
+        return"form";
+    }
+
+    @GetMapping("/index")
+    public String controladores6(){
+        return"index";
+    }
+
+    @GetMapping("/project-description")
+    public String controladores7(){
+        return"project-description";
+    }
+
+    @GetMapping("/requirements")
+    public String controladores8(){
+        return"requirements";
+    }
+
+    @GetMapping("/test")
+    public String controladores9(){
+        return"test";
     }
 
 }
