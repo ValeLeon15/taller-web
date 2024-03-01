1103c955527fceaf9692f6bac8e8f4de19f141da
ValeLeon15 <vlg1529@gmail.com>
2024-03-01 12:43:47 -0500

    controles

---
+++ src/main/java/com/example/demo/controllers/paraDesarrolloController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/grupo14/control")
+public class paraDesarrolloController {
+
+    @GetMapping("/paraDesarrollo")
+    public String paraDesarrollo(){
+        return"paraDesarrollo.html";
+    }
+}
---
+++ src/main/java/com/example/demo/controllers/requerimientosController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/grupo14/control")
+public class requerimientosController {
+    @GetMapping("/requerimientos")
+    public String requerimientos(){
+        return"requerimientos.html";
+    }
+}
